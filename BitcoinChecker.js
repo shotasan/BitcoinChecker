4dd2065e5d53e03b0b64e297785d053e1eb1e09e
shotasan <wondermoai@gmail.com>
2019-10-15 14:56:55 +0900

    Add: 各通貨のレート表示機能の実装

--- BitcoinChecker.js
+++ BitcoinChecker.js
@@ -1,14 +1,22 @@
 new Vue({
   el: "#app",
   data: {
-    bpi: null
+    bpi: null,
   },
   mounted: function () {
     axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(function (response) {
-        // console.log(response.data.bpi)
-        // console.log(response.data.bpi.USD.rate_float)
+        // bindが無いとthisはwindowオブジェクトを指す。bind(this)によりthisがvueインスタンスになる。
+        // this.bpiでdataオブジェクトのbpiプロパティに値を設定する。
         this.bpi = response.data.bpi
       }.bind(this))
+      .catch(function (error) {
+        console.log(error)
+      }.bind(this))
+  },
+  filters: {
+    currencyDecimal(value) {
+      return value.toFixed(2)
+    }
   }
 })
